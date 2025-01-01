242d6ace4f3521909c183b13af12166125568fb8
jayappideas <jaypatel.theappideas@gmail.com>
2025-01-01 11:22:39 +0530

    remove unnessory code.

--- controllers/api/cmsController.js
+++ controllers/api/cmsController.js
@@ -3,20 +3,6 @@
 const faqModel = require('../../models/faqModel');
 const pageModel = require('../../models/pageModel');
 
-
-function multilingualPage(doc, req) {
-    const accepted = ['en', 'ar'];
-    let language = accepted.includes(req.headers['accept-language'])
-        ? req.headers['accept-language']
-        : 'en';
-    const lang = doc[language];
-    let spread = doc.toObject ? doc.toObject() : doc;
-    const newDoc = { ...lang, ...spread };
-    delete newDoc.en;
-    delete newDoc.ar;
-    return newDoc;
-}
-
 exports.termCondition = async (req, res, next) => {
     try {
         let page = await pageModel.findOne({ key: "term" }).select('-__v -key -_id');
